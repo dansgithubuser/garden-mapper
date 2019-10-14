218ff82957fe2792734f59c5a530a634caf427c0
Dan <dansonlinepresence@gmail.com>
2019-10-14 16:01:27 -0400

    add wslog

---
+++ garden_mapper/static/garden_mapper/wslog.js
@@ -0,0 +1,14 @@
+const enabled = 0;
+
+var wslog = () => {};
+
+if (enabled) {
+  const params = new URLSearchParams(window.location.search);
+  const hostname = params.get('wslog_hostname') || window.location.hostname;
+  const socket = new WebSocket(`ws://${hostname}:9111`);
+  wslog = function() {
+    socket.send(Array.from(arguments).map(i => JSON.stringify(i)).join(' '));
+  }
+}
+
+export { wslog };
